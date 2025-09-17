ca7474599202e0435a6d786f2c2166c5e2da6e36
olivia-l0803 <olivialu415@gmail.com>
2025-09-16 23:21:28 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 print("Yay!!! You completed the game!! You will get FREE private tickets to the turtle show! :-P")
 
 import turtle
+import random
 turtle.shape("turtle")
 
 def star(length):
@@ -9,3 +10,22 @@ def star(length):
         turtle.right(144)
 
 star(50)
+
+import turtle
+turtle.shape("turtle")
+
+def star(length):
+    for i in range(5):
+        turtle.forward(length)
+        turtle.right(144)
+
+def starspiral(length, iterate):
+    for i in range(iterate):
+        star(length)
+        turtle.right(5)
+        length += 5
+
+while True:
+    turtle.speed(random.randint(20,100))
+    starspiral(random.randint(3, 30), random.randint(40,100))
+    turtle.clear()
