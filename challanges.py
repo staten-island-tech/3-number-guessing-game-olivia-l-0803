d32e67d5a6d05965b8a0997f493fb8494ba0831a
olivia l <olivialu415@gmail.com>
2025-09-22 10:17:09 -0400

    Update challanges.py

--- challanges.py
+++ challanges.py
@@ -3,3 +3,11 @@ countdown = 10
 while countdown != 0:
     print(countdown)
     countdown = countdown - 1
+
+
+consent = True
+print("Im gonna tickle your feet....")
+while consent == True:
+    reply = input("Tickle!!!! ").lower
+    if reply == "no" or reply == "stop":
+        consent == False
